e8198657b5349ad4eab61f8f6d6e121bfe04da24
Gbemmiey <gbemfemalabi@gmail.com>
2022-04-26 16:18:44 +0100

    Update:

---
+++ index.js
@@ -0,0 +1,10 @@
+var contact = document.getElementById("contactPage");
+var about = document.getElementById("aboutPage");
+var heroText = document.getElementsByClassName("hero-text");
+
+
+let toggle = function () {
+  contact.addEventListener(onclick);
+  about.addEventListener(onclick);
+  
+};
